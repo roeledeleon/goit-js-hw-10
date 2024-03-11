41e5acc0f465597692ca81e3f8f97cac6f9c68b0
roel.edeleon <roel.edeleon@gmail.com>
2024-03-11 21:47:38 +0800

    Commit 202303211 2147H
    update index.js
    update styles.css

--- src/index.js
+++ src/index.js
@@ -1,59 +1,93 @@
+// ---------- IMPORTS
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+import './styles.css';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// ---------- DECLARATIONS
 const breedSelectEl = document.querySelector('.breed-select');
 const catInfoEl = document.querySelector('.cat-info');
 const loaderEl = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
 
-let breedId = '';
-
+breedSelectEl.style.display = 'none';
 catInfoEl.style.display = 'none';
 loaderEl.style.display = 'none';
 errorEl.style.display = 'none';
 
-//CREATE THE OPTIONS
+// ---------- FUNCTIONS
+let breedId = '';
+
 function chooseBreed() {
-  fetchBreeds().then(data => {
-    //console.log(data);
-    let optionsMarkup = data.map(({ name, id }) => {
-      return `<option value=${id}>${name}</option>`;
-    });
-    breedSelectEl.insertAdjacentHTML('beforeend', optionsMarkup);
-  });
+  loaderEl.style.display = 'block';
+  fetchBreeds()
+    .then(data => {
+      //console.log(data);
+      let optionsMarkup = data.map(({ name, id }) => {
+        return `<option value=${id}>${name}</option>`;
+      });
+      breedSelectEl.insertAdjacentHTML('beforeend', optionsMarkup);
+      loaderEl.style.display = 'none';
+      breedSelectEl.style.display = 'block';
+    })
+    .catch(onError);
 }
 
 chooseBreed();
 
 breedSelectEl.addEventListener('change', e => {
+  e.preventDefault();
+  loaderEl.style.display = 'block';
   let breedId = e.target.value;
   fetchCatByBreed(breedId)
     .then(data => {
       const { url, breeds } = data[0];
       const { name, description, temperament } = breeds[0];
 
-      //console.log(data[0]);
-      //console.log(breeds[0]);
-
-      catInfoEl.style.display = 'block';
-
       catInfoEl.innerHTML = `
-      <br />
-      <img src='${url}' alt='${name}' width="400" />
-      <div class='box'>
-        <h2>${name}</h2>
-        <p>${description}</p>
-        <p>${temperament}</p>
+        <br />
+        <div class="box-img">
+          <img src="${url}" alt="cat" width="500"/>
+        </div>
+       
+        <div class="box">
+          <h1>${name}</h1>
+          <p>${description}</p>
+          <p><b>Temperament:</b> ${temperament}</p>
+        </div>
         `;
+
+      catInfoEl.style.display = 'flex';
+      loaderEl.style.display = 'none';
     })
     .catch(onError);
 });
 
 function onError(error) {
+  //errorEl.style.display = 'block';
   Notiflix.Notify.warning(
     'Oops! Something went wrong! Try reloading the page!'
   );
 }
+
+// function chooseBreed() {
+//   let arrBreedsId = [];
+//   breedSelectEl.style.display = 'block';
+//   fetchBreeds()
+//     .then(data => {
+//       data.forEach(element => {
+//         arrBreedsId.push({ text: element.name, value: element.id });
+//       });
+//       console.log(arrBreedsId);
+//       new SlimSelect({
+//         select: breedSelectEl,
+//         data: arrBreedsId,
+//         placeholder: 'Search',
+//         showSearch: false, // shows search field
+//         searchText: 'Sorry couldnt find anything',
+//       });
+//     })
+//     .catch(onError);
+// }
